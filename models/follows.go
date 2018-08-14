1ecfcc6bde4bbf7f516463d0d25e49abaef15d75
Yen <yychen@mirrormedia.mg>
2018-08-14 16:32:38 +0800

    1. Count pageview when updating hot tags 2. Adding related resources to the result of getting following tags by user

--- models/follows.go
+++ models/follows.go
@@ -80,6 +80,7 @@ func (g *GetFollowingArgs) scan(rows *sqlx.Rows) (interface{}, error) {
 	var (
 		followings []interface{}
 		err        error
+		IDs        []int
 	)
 
 	for rows.Next() {
@@ -108,6 +109,7 @@ func (g *GetFollowingArgs) scan(rows *sqlx.Rows) (interface{}, error) {
 		case "tag":
 			var tag Tag
 			err = rows.StructScan(&tag)
+			IDs = append(IDs, tag.ID)
 			followings = append(followings, tag)
 		default:
 			return nil, errors.New("Unsupported Resource")
@@ -117,6 +119,25 @@ func (g *GetFollowingArgs) scan(rows *sqlx.Rows) (interface{}, error) {
 			log.Println(err.Error())
 			return followings, err
 		}
+
+		if g.ResourceName == "tag" {
+			tagDetails, err := TagAPI.GetTags(GetTagsArgs{
+				ShowStats:     true,
+				ShowResources: true,
+				IDs:           IDs,
+				PostFields:    sqlfields{"post_id", "publish_status", "published_at", "title", "type"},
+				ProjectFields: sqlfields{"project_id", "publish_status", "published_at", "title", "slug", "status"},
+			})
+			if err != nil {
+				log.Println("Error getting tag info when updating hottags:", err)
+				return nil, err
+			}
+
+			for _, tag := range tagDetails {
+				followings = append(followings, tag)
+			}
+		}
+
 	}
 	return followings, err
 }
