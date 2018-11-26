a066c8ef2da67ea553eb2e9e5e94dda23d2a34fc
I-Chiao Hsu <ichiao.hsu@gmail.com>
2018-11-26 11:22:00 +0800

    Create, Retrieve, Update functions for polls and choices

---
+++ poll/filter.go
@@ -0,0 +1,63 @@
+package poll
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+// GetPollsFilter is used to store GET filters
+type GetPollsFilter struct {
+	MaxResults      int    `form:"max_results"`
+	Page            int    `form:"page"`
+	OrderBy         string `form:"order_by"`
+	EmbeddedChoices bool   `form:"embedded_choices"`
+}
+
+func (f *GetPollsFilter) validate() bool {
+	// TODO: validator for each filters
+	return true
+}
+
+// Parse could return a function to modify SQLO object with fields in GetPollsFilter
+func (f *GetPollsFilter) Parse() func(s *SQLO) {
+	return func(s *SQLO) {
+
+		if f.EmbeddedChoices {
+			s.join = append(s.join, " LEFT JOIN polls_choices AS choice ON polls.id = choice.poll_id")
+			s.fields = append(s.fields, sqlfield{table: "choice", pattern: `%s.%s "%s.%s"`, fields: GetStructTags("full", "db", Choice{})})
+		}
+		if f.MaxResults != 0 && f.Page > 0 {
+			s.pagination = fmt.Sprintf(" LIMIT %d OFFSET %d", f.MaxResults, f.Page-1)
+		}
+		if f.OrderBy != "" {
+			s.FormatOrderBy(f.OrderBy)
+		}
+	}
+}
+
+// SetGetPollsFilter is the constructor for GetPollsFilter, exploited in router
+// Default values are  MaxResults = 20, Page = 1, OrderBy = -updated_at
+func SetGetPollsFilter(options ...func(*GetPollsFilter) (err error)) (*GetPollsFilter, error) {
+
+	args := GetPollsFilter{MaxResults: 20, Page: 1, OrderBy: "-created_at"}
+
+	for _, option := range options {
+		if err := option(&args); err != nil {
+			return nil, err
+		}
+	}
+	return &args, nil
+}
+
+// BindQuery is used in NewRouterFilter, so it has corresponding variable fingerprints.
+// Router binds all query parameters here, including all the customized parameter forms.
+func BindQuery(c *gin.Context) func(*GetPollsFilter) error {
+
+	return func(f *GetPollsFilter) (err error) {
+		if err = c.ShouldBindQuery(f); err != nil {
+			fmt.Println(err.Error())
+		}
+		return err
+	}
+}
---
+++ poll/router.go
@@ -0,0 +1,150 @@
+package poll
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type router struct{}
+
+// GetPolls returns list of polls. If filter provided,
+// it could return polls with corresponding choices
+func (r *router) GetPolls(c *gin.Context) {
+
+	filter, err := SetGetPollsFilter(BindQuery(c))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+		return
+	}
+	results, err := PollData.Get(filter)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"_items": results})
+}
+
+// PostPolls accepts a single poll w/o choices
+// and create new poll using input data
+func (r *router) PostPolls(c *gin.Context) {
+
+	poll := ChoicesEmbeddedPoll{}
+	if err := c.Bind(&poll); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+		return
+	}
+	// ----------- Start of input validation
+	poll.Poll.Validate(
+		ValidatePollInsertID,
+		ValidatePollCreatedAt,
+		ValidatePollUpdatedAt,
+	)
+	// Validate attached choices
+	for i := range poll.Choices {
+		poll.Choices[i].Validate(
+			ValidateChoiceInsertID,
+			ValidateChoiceCreatedAt,
+			ValidateChoiceUpdatedAt,
+		)
+	} // --------- End of input validation
+	if err := PollData.Insert(poll); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Error": err.Error()})
+		return
+	}
+	c.Status(http.StatusCreated)
+}
+
+// PutPolls updates contents of a single poll
+func (r *router) PutPolls(c *gin.Context) {
+
+	poll := Poll{}
+	if err := c.Bind(&poll); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+		return
+	}
+	// validation sections
+	poll.Validate(ValidatePollUpdatedAt)
+	err := PollData.Update(poll)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Error": err.Error()})
+		return
+	}
+	c.Status(http.StatusNoContent)
+}
+
+func (r *router) GetChoices(c *gin.Context) {
+
+	pollID, _ := strconv.ParseInt(c.Param("id"), 10, 64)
+	results, err := ChoiceData.Get(int(pollID))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"_items": results})
+}
+
+func (r *router) InsertChoices(c *gin.Context) {
+
+	pollID, _ := strconv.ParseInt(c.Param("id"), 10, 64)
+	var input struct {
+		Choices []Choice `json:"choices"`
+	}
+	if err := c.Bind(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+		return
+	}
+	// ------------ Start of input validation
+	for i := range input.Choices {
+		input.Choices[i].Validate(
+			ValidateChoiceInsertID,
+			ValidateChoiceCreatedAt,
+			ValidateChoicePollID(pollID))
+	} // ---------- End of input validation
+	if err := ChoiceData.Insert(input.Choices); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Error": err.Error()})
+		return
+	}
+	c.Status(http.StatusCreated)
+}
+
+func (r *router) PutChoices(c *gin.Context) {
+
+	pollID, _ := strconv.ParseInt(c.Param("id"), 10, 64)
+	var input struct {
+		Choices []Choice `json:"choices"`
+	}
+	if err := c.Bind(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+		return
+	}
+	for i := range input.Choices {
+		input.Choices[i].Validate(
+			ValidateChoicePollID(pollID),
+			ValidateChoiceUpdatedAt,
+		)
+	}
+	if err := ChoiceData.Update(input.Choices); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Error": err.Error()})
+		return
+	}
+	c.Status(http.StatusNoContent)
+}
+
+func (r *router) SetRoutes(router *gin.Engine) {
+
+	v2 := router.Group("/v2/polls")
+	{
+		v2.GET("", r.GetPolls)
+		v2.POST("", r.PostPolls)
+		v2.PUT("", r.PutPolls)
+
+		v2.GET("/:id/choices", r.GetChoices)
+		v2.POST("/:id/choices", r.InsertChoices)
+		v2.PUT("/:id/choices", r.PutChoices)
+	}
+}
+
+// Router is the single routing instance used in registration in routes/routes.go
+var Router router
