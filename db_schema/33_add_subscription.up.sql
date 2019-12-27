b0096da7b58e7c0d6a04242e32f8853a2f631686
I-Chiao Hsu <ichiao.hsu@gmail.com>
2019-12-27 14:40:08 +0800

    refactor(subscription): modified schema - member_id set to nullable - included email field - added member_id as foreign key

--- db_schema/33_add_subscription.up.sql
+++ db_schema/33_add_subscription.up.sql
@@ -1,7 +1,8 @@
 -- Create new table subscriptions
 CREATE TABLE IF NOT EXISTS `subscriptions` (
     `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-    `member_id` bigint(20) unsigned NOT NULL UNIQUE,
+    `member_id` bigint(20) unsigned DEFAULT NULL,
+    `email` varchar(32) NOT NULL DEFAULT '',
     `amount` int(11) NOT NULL,
     `payment_service` varchar(32) NOT NULL DEFAULT '',
     `invoice_service` varchar(32) NOT NULL DEFAULT '',
@@ -11,5 +12,10 @@ CREATE TABLE IF NOT EXISTS `subscriptions` (
     `status` int(11) NOT NULL DEFAULT 0,
     `payment_infos` JSON,
     `invoice_infos` JSON,
-    PRIMARY KEY(`id`)
+    
+    PRIMARY KEY(`id`),
+    INDEX (member_id),
+
+    FOREIGN KEY (member_id)
+        REFERENCES members (id) ON UPDATE CASCADE ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- pkg/subscription/subscription.go
+++ pkg/subscription/subscription.go
@@ -7,8 +7,6 @@ import (
 	"github.com/readr-media/readr-restful/internal/rrsql"
 )
 
-// TODO: define ENUM status
-
 // PaymentStore wraps the necessary data structure for sqlx
 // gin could bind map[string]interface{} directly, but not sqlx
 type PaymentStore map[string]interface{}
@@ -47,15 +45,16 @@ type Subscriber interface {
 
 // Subscription is the model for unmarshalling JSON, and serialized to database
 type Subscription struct {
-	ID             uint64         `json:"id,omitempty" db:"id"`                                  // Subscription id
-	MemberID       int64          `json:"member_id,omitempty" db:"member_id" binding:"required"` // Member who subscribed
-	Amount         int            `json:"amount,omitempty" db:"amount" binding:"required,gt=0"`  // Amount to pay
-	CreatedAt      rrsql.NullTime `json:"created_at,omitempty" db:"created_at"`                  // The time when first created
-	UpdatedAt      rrsql.NullTime `json:"updated_at,omitempty" db:"updated_at"`                  // The time when renewal
-	LastPaidAt     rrsql.NullTime `json:"last_paid_at,omitempty" db:"last_paid_at"`              // Last time paid
-	PaymentService string         `json:"payment_service,omitempty" db:"payment_service"`        // Payment service name
-	InvoiceService string         `json:"invoice_service,omitempty" db:"invoice_service"`        // Invoice service name
-	Status         int            `json:"status" db:"status"`
+	ID             uint64         `json:"id,omitempty" db:"id"`                                 // Subscription id
+	MemberID       rrsql.NullInt  `json:"member_id,omitempty" db:"member_id"`                   // Member who subscribed
+	Email          string         `json:"email,omitempty" db:"email" binding:"required"`        // Email for failure handle, invoice create
+	Amount         int            `json:"amount,omitempty" db:"amount" binding:"required,gt=0"` // Amount to pay
+	CreatedAt      rrsql.NullTime `json:"created_at,omitempty" db:"created_at"`                 // The time when first created
+	UpdatedAt      rrsql.NullTime `json:"updated_at,omitempty" db:"updated_at"`                 // The time when renewal
+	LastPaidAt     rrsql.NullTime `json:"last_paid_at,omitempty" db:"last_paid_at"`             // Last time paid
+	PaymentService string         `json:"payment_service,omitempty" db:"payment_service"`       // Payment service name
+	InvoiceService string         `json:"invoice_service,omitempty" db:"invoice_service"`       // Invoice service name
+	Status         int            `json:"status,omitempty" db:"status"`
 	PaymentInfos   PaymentStore   `json:"payment_infos,omitempty" db:"payment_infos"`
 	InvoiceInfos   InvoiceStore   `json:"invoice_infos,omitempty" db:"invoice_infos"`
 }
